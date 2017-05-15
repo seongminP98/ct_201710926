4a54ec244fa1050d9630f664edf32584c27f07ba
sungmin98 <psm7335@naver.com>
2017-05-15 17:39:39 +0900

    Add files via upload

---
+++ Prac11_04.py
@@ -0,0 +1,12 @@
+﻿from PIL import Image 
+m = Image.open("C:\\Users\\400T6B\\Downloads\\italy-1587287_1920.jpg")
+m.show()
+
+from matplotlib.pyplot import imshow
+imshow(m)
+
+m.save("C:\\Users\\400T6B\\Downloads\\italy-1587287_1920.gif")
+mygif=Image.open("C:\\Users\\400T6B\\Downloads\\italy-1587287_1920.gif")
+
+from matplotlib.pyplot import imshow
+imshow(mygif)
