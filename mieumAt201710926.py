763d081195f789f20e6fa749473d019d6632f4a2
sungmin98 <psm7335@naver.com>
2017-05-22 17:03:11 +0900

    Add files via upload

---
+++ mieumAt201710926.py
@@ -0,0 +1,34 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+
+def giyukAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.fd(size)
+    t1.right(90)
+    t1.fd(size)
+
+def nieunAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.right(90)
+    t1.fd(size)
+    t1.left(90)
+    t1.fd(size)
+
+def mieumAt(x,y,size):
+    giyukAt(x,y,size)
+    t1.penup()
+    t1.goto(x,y)
+    t1.left(90)
+    t1.pendown()
+    nieunAt(x,y,size)
+
+mieumAt(-300,300,100)
+
+
+wn.exitonclick()
