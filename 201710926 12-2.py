58ff156fba34bcf0caccd1103d442bd40eccb765
sungmin98 <psm7335@naver.com>
2017-05-29 17:03:31 +0900

    Add files via upload

---
+++ 201710926 12-2.py
@@ -0,0 +1,44 @@
+﻿import turtle
+wn = turtle.Screen()
+t1 = turtle.Turtle()
+
+def drawSquare(size):
+    for i in range(0,4):
+        t1.fd(size)
+        t1.right(90)
+        
+def drawSquareAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    drawSquare(size)
+
+
+def drawTriangle(size):
+    for i in range(0,3):
+        t1.fd(size)
+        t1.right(120)
+        
+def drawTriangleAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    drawTriangle(size)
+
+
+def drawStar(size):
+    for i in range(0,5):
+        t1.fd(size)
+        t1.right(144)
+        
+def drawStarAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    drawStar(size)
+
+drawSquareAt(-200,-200,100)
+drawTriangleAt(0,0,100)
+drawStarAt(200,200,100)
+
+wn.exitonclick()
